ac6e136260bc306535ede7443ee9514feda7e288
DH <dennis.kunny.t.p@gmail.com>
2021-12-16 10:29:53 +0100

    update declaration

--- nextColor.js
+++ nextColor.js
@@ -1,8 +1,13 @@
+// set a var with 3 colors as 3 items
 var colors = ["red", "blue", "yellow"];
 	
 function nextColor()
 {
+	// choose a color by returning integer number of product
+	// of 3 and a number between 0 and 1
 	selectedColor = colors[Math.floor(Math.random()*colors.length)];
+	
+	// change body style backgroundColor with the choosen text
 	document.body.style.backgroundColor = selectedColor;
 	// nextColor(false);
 }
