6ecb54a464275fda75d15f05a2f2727c8a16cd64
DH <dennis.kunny.t.p@gmail.com>
2021-12-16 11:44:13 +0100

    Update function

--- msg.js
+++ msg.js
@@ -1,15 +1,13 @@
-var x;
+const x = document.getElementById("input");;
 function send()
 {
-	x = document.getElementById("input");
 	//$('#input').val('');
 	document.getElementById("show").innerHTML = x.value;
 	document.getElementById("input").value = '';
 	//$('#input').val('');
 }
 
-var input = document.getElementById("input");
-input.addEventListener("keyup", function(event) {
+x.addEventListener("keyup", function(event) {
 	if (event.keyCode === 13) {
 		event.preventDefault();
 		document.getElementById("btn").click();
