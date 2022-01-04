b2fc2ff80ddaa2c1a91db21e94e1e5931752bbbe
DH <dennis.kunny.t.p@gmail.com>
2022-01-04 22:59:09 +0100

    Update

--- msg.js
+++ msg.js
@@ -1,14 +1,19 @@
-const x = document.getElementById("input");;
+const input = document.getElementById("input");
+const btn = document.getElementById("btn");
+
 function send()
 {
-	//$('#input').val('');
-	document.getElementById("show").innerHTML = x.value;
+	// show input
+	document.getElementById("show").innerHTML = input.value;
+	// and delete the field
 	document.getElementById("input").value = '';
-	//$('#input').val('');
 }
+// create event 'click' for Element with id 'btn' that fire function 'send'
+btn.addEventListener('click',send);
 
-x.addEventListener("keyup", function(event) {
-	if (event.keyCode === 13) {
+// create event 'keyup' with Enter for Element with id 'input' that fire function 'send'
+input.addEventListener("keyup", function(event) {
+	if (event.keyCode === 13) { //keyCode 13 is key Enter
 		event.preventDefault();
 		document.getElementById("btn").click();
 	}
