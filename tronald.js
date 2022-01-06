b112d4986061270f5dd85270094d29dc2135ec48
DH <dennis.kunny.t.p@gmail.com>
2022-01-06 14:00:47 +0100

    update

--- tronald.js
+++ tronald.js
@@ -1,5 +1,6 @@
 let celeb = document.getElementById('celeb');
 let quote = document.getElementById('quote');
+let btn = document.getElementById('btn');
 var ask;
 
 function ask()
@@ -10,3 +11,20 @@ function ask()
 	ask.open("GET","https://api.tronalddump.io/random/quote",true);
 	ask.send();
 }
+
+function alertContents()
+{
+    if (ask.readyState === XMLHttpRequest.DONE)
+	{
+      if (ask.status === 200)
+	  {
+        alert(ask.responseText);
+      } 
+	  else 
+	  {
+        alert('There was a problem with the request.');
+      }
+    }
+}
+
+btn.addEventListener('click', ask);
