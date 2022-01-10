611037540889b37a989cc5133404ddc446213e59
DH <dennis.kunny.t.p@gmail.com>
2022-01-10 11:22:53 +0100

    finish

--- tronald.js
+++ tronald.js
@@ -1,12 +1,15 @@
-let celeb = document.getElementById('celeb');
+let target = document.getElementById('target');
 let quote = document.getElementById('quote');
 let btn = document.getElementById('btn');
 var ask;
 
 function ask()
 {
+	// fetch('https://api.tronalddump.io/random/quote')
+		// .then(response => response.json())
+		// .then(data => alert(data.value));
+
 	ask = new XMLHttpRequest();
-	
 	ask.onreadystatechange = alertContents;
 	ask.open("GET","https://api.tronalddump.io/random/quote",true);
 	ask.send();
@@ -16,14 +19,16 @@ function alertContents()
 {
     if (ask.readyState === XMLHttpRequest.DONE)
 	{
-      if (ask.status === 200)
-	  {
-        alert(ask.responseText);
-      } 
-	  else 
-	  {
-        alert('There was a problem with the request.');
-      }
+		if (ask.status === 200)
+		{
+			var jsonResponse = JSON.parse(ask.responseText);
+			target.innerHTML = jsonResponse.tags;
+			quote.innerHTML = jsonResponse.value;
+		} 
+		else 
+		{
+			alert('There was a problem with the request.');
+		}
     }
 }
 
